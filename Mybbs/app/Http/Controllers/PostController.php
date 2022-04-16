757d61bb3c870868b29c7082fa1978d073cf5814
yoshihiro <akatukijun@gmail.com>
2022-04-16 13:30:45 +0900

    edit

--- Mybbs/app/Http/Controllers/PostController.php
+++ Mybbs/app/Http/Controllers/PostController.php
@@ -24,4 +24,28 @@ class PostController extends Controller
     {
         return view('posts.create');
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+            'body'  => 'required',
+        ], [
+            'title.required' => 'タイトルは必須です。',
+            'title.min' => ':min 文字以上入力してください',
+            'body.required'  => '本文は必須です。',
+        ]);
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()
+            ->route('posts.index');
+    }
+    public function edit(Post $post)
+    {
+        return view('posts.edit')
+            ->with(['post' => $post]);
+    }
+
 }
